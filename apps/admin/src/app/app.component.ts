4477d2f3fca23db061251d49b8d8bdfcc9954eaa
siranjeevi <ksiranjeevi92@gmail.com>
2024-08-03 23:36:05 +0530

    created lib

--- apps/admin/src/app/app.component.ts
+++ apps/admin/src/app/app.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, HeaderComponent, FooterComponent],
+  imports: [NxWelcomeComponent, RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'ngshop';
+  title = 'admin';
 }
