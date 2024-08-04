7652ea9d69fe84fb96c9b5fb09c4625feed19a75
siranjeevi <ksiranjeevi92@gmail.com>
2024-08-04 19:36:46 +0530

    admin sidebar

---
+++ apps/admin/src/app/shared/shell/shell.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'admin-shell',
+  standalone: true,
+  imports: [],
+  templateUrl: './shell.component.html',
+})
+export class ShellComponent {}
---
+++ apps/admin/src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'admin-sidebar',
+  standalone: true,
+  imports: [],
+  templateUrl: './sidebar.component.html',
+})
+export class SidebarComponent {}
